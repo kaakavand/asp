c55c5a6a746f06a2e91e51c4913bf056ed2f7014
Amirhosein Kakavand <94612720+kaakavand@users.noreply.github.com>
2023-09-04 20:16:30 +0330

    fix layout

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-4 h-full sm:flex-row">
+        <section className='bg-white rounded-lg shadow-md w-full'>
+          amirhossein
+        </section>
+        <section className='bg-white rounded-lg shadow-md w-full'>
+          amirhossein
+        </section>
+    </main>
+  )
+}
